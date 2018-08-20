c22e0da32ebb8f63af4f77e0c8f73e0f9de4c9ca
kei <apribase@gmail.com>
2018-08-21 02:02:53 +0900

    Deploy 0.10.5 to Clojars.

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject lein-sassc "0.10.5-SNAPSHOT"
+(defproject lein-sassc "0.10.5"
   :description "Leiningen plugin to compile SASS/SCSS files with SassC."
   :url "http://bitbucket.org/apribase/lein-sassc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "https://github.com/apribase/lein-sassc.git"}
+  :repositories [["releases" {:url "https://github.com/apribase/lein-sassc"
+                              :creds :gpg}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [me.raynes/fs "1.4.6"]]
   :hooks [leiningen.sassc]
